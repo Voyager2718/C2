85c04125c949ddd99bd92787bb04e61fb38f4d02
Voyager2718 <voyager2718@gmail.com>
2016-03-20 00:08:52 +0100

    C2 Poképet

---
+++ app/src/main/java/com/zhipengyang/c2/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.zhipengyang.c2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Toast;
+
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        setContentView(R.layout.settings);
+        setTitle(R.string.settings);
+        Toast.makeText(getApplicationContext(), "Settings Activity", Toast.LENGTH_LONG).show();
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}

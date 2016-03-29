d7eb98ce0a68765a864a719897826a6f65384c8d
Voyager2718 <voyager2718@gmail.com>
2016-03-29 22:43:43 +0200

    C2 the bunny UI preview 1

---
+++ BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/MainActivity.java
@@ -0,0 +1,137 @@
+package com.zhipengyang.bunnyc2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab;
+        if ((fab = (FloatingActionButton) findViewById(R.id.fab)) == null) {
+            Toast.makeText(getApplicationContext(), "Error code: 0x0001", Toast.LENGTH_LONG).show();
+            return;
+        }
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, getString(R.string.contact_author), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView == null) {
+            Toast.makeText(getApplicationContext(), "Error code: 0x0002", Toast.LENGTH_LONG).show();
+            return;
+        }
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer == null) {
+            Toast.makeText(getApplicationContext(), "Error code: 0x0003", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_city) {
+            Toast.makeText(getApplicationContext(), "City (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.school) {
+            Toast.makeText(getApplicationContext(), "School (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.hospital) {
+            Toast.makeText(getApplicationContext(), "Hospital (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.park) {
+            Toast.makeText(getApplicationContext(), "Park (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.restaurant) {
+            Toast.makeText(getApplicationContext(), "Restaurant (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.microsoft_research) {
+            Toast.makeText(getApplicationContext(), "Microsoft Research (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.google_corp) {
+            Toast.makeText(getApplicationContext(), "Google Corp (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.rotten_apple) {
+            Toast.makeText(getApplicationContext(), "Rotten apple Corp (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.abandoned_house) {
+            Toast.makeText(getApplicationContext(), "Abandoned house (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_home) {
+            Toast.makeText(getApplicationContext(), "Home (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(getApplicationContext(), "Gallery (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_shop) {
+            Toast.makeText(getApplicationContext(), "Shop (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(getApplicationContext(), "Share (Not implemented yet)", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_send) {
+            Toast.makeText(getApplicationContext(), "Send (Not implemented yet)", Toast.LENGTH_LONG).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer == null) {
+            Toast.makeText(getApplicationContext(), "Error code: 0x0004", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/SettingsActivity.java
@@ -0,0 +1,38 @@
+package com.zhipengyang.bunnyc2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        setTitle(R.string.settings);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Not been implemented yet.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}

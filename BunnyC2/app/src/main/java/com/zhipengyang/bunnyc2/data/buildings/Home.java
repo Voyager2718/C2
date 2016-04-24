182dcef8d675ee3d52cd194c5c6a6de87630cde6
Voyager2718 <voyager2718@gmail.com>
2016-04-24 23:53:27 +0200

    Rename, use singleton.

--- BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/data/buildings/Home.java
+++ BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/data/buildings/Home.java
@@ -7,8 +7,19 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Home {
+public class Home implements IBuilding {
+    private static volatile Home INSTANCE = null;
+
     List<IEquipment> equipments = new LinkedList<>();
 
     List<ACharacter> characters = new ArrayList<ACharacter>();
+
+    public static Home getInstance() {
+        if (INSTANCE == null)
+            synchronized (Home.class) {
+                if (INSTANCE == null)
+                    INSTANCE = new Home();
+            }
+        return INSTANCE;
+    }
 }
---
+++ BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/data/buildings/IBuilding.java
@@ -0,0 +1,4 @@
+package com.zhipengyang.bunnyc2.data.buildings;
+
+public interface IBuilding {
+}

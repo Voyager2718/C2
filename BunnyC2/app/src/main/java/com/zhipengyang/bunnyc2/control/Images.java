5aa5f7753b0043fe12236e657b9b95d81519b172
Voyager2718 <voyager2718@gmail.com>
2016-04-27 21:38:45 +0200

    Improve resize(); GOOD VERSION

--- BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/control/Images.java
+++ BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/control/Images.java
@@ -4,11 +4,17 @@ import android.graphics.Bitmap;
 import android.graphics.Matrix;
 
 public class Images {
+    /**
+     * @param bitmap: Bitmap to resize.
+     * @param width:  Don't resize if width = 0
+     * @param height: Don't resize if height = 0
+     * @return: Bitmap resized
+     */
     public static Bitmap resizeBitmap(Bitmap bitmap, int width, int height) {
         if (bitmap == null)
             return null;
         int bitmapWidth = bitmap.getWidth(), bitmapHeight = bitmap.getHeight();
-        float scaleWidth = ((float) width) / bitmapWidth, scaleHeight = ((float) height) / bitmapHeight;
+        float scaleWidth = width != 0 ? ((float) width) / bitmapWidth : 1f, scaleHeight = height != 0 ? ((float) height) / bitmapHeight : 1f;
         Matrix matrix = new Matrix();
         matrix.postScale(scaleWidth, scaleHeight);
         return Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);

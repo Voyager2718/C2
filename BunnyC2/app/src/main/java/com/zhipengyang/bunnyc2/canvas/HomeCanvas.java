fc2ab89e3593a2b08916e9d5322e0a11832c7c84
Voyager2718 <voyager2718@gmail.com>
2016-05-07 23:05:43 +0200

    Animation with postInvaildDelay(250)

--- BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/canvas/HomeCanvas.java
+++ BunnyC2/app/src/main/java/com/zhipengyang/bunnyc2/canvas/HomeCanvas.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.zhipengyang.bunnyc2.R;
 import com.zhipengyang.bunnyc2.control.Images;
@@ -22,6 +23,10 @@ public class HomeCanvas extends View {
     private List<Bitmap> character = new ArrayList<>();
     private int characterFlame = 0;
     private int flameDirection = -1;
+    private int flames = 0;
+    private int characterFlamesPerSecond = 4;
+
+    private ImageView characterAnimationImageView;
 
     private Canvas canvas;
     private Paint paintBrush;
@@ -49,6 +54,7 @@ public class HomeCanvas extends View {
         character.add(BitmapFactory.decodeResource(getResources(), R.drawable.c2_hand4));
 
         bitmaps.add(BitmapFactory.decodeResource(getResources(), R.drawable.home));
+        bitmaps.add(BitmapFactory.decodeResource(getResources(), R.drawable.c2));
 
         animationThread = new Thread(new Runnable() {
             @Override
@@ -63,7 +69,38 @@ public class HomeCanvas extends View {
         super.onDraw(canvas);
         this.canvas = canvas;
 
-        canvas.drawBitmap(Images.resizeBitmap(bitmaps.get(0), canvas.getWidth(), canvas.getHeight()), 0, 0, null);
+        canvas.drawBitmap(Images.resizeBitmap(bitmaps.get(0), canvas.getWidth(), canvas.getHeight()), 0, 0, null);//Draw background
+        //canvas.drawBitmap(bitmaps.get(1), canvas.getWidth() / 4, canvas.getHeight() / 4, null);//Draw C2
+
+        /*
+        //animate with invalidate();
+        //flames = flames >= 1000 ? 0 : flames + 10;
+        //if (flames % (1000 / 4) == 0) {
+        if (characterFlame >= character.size() - 1 || characterFlame <= 0) {
+            flameDirection *= -1;
+            characterFlame = characterFlame + flameDirection;
+        } else
+            characterFlame += flameDirection;
+        //The structure above can reverse the direction of animation.
+        canvas.drawBitmap(character.get(characterFlame), canvas.getWidth() / 4, canvas.getHeight() / 4, null);
+        //}
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            Thread.interrupted();
+        }
+
+        invalidate();
+        */
+
+
+        /*
+        //Draw a lightgray rectangle
+        paintBrush.setColor(Color.parseColor("lightgray"));
+        paintBrush.setStrokeWidth(10);
+        canvas.drawRect(100, 100, 800, 800, paintBrush);
+        */
 
         if (characterFlame >= character.size() - 1 || characterFlame <= 0) {
             flameDirection *= -1;
@@ -72,19 +109,7 @@ public class HomeCanvas extends View {
             characterFlame += flameDirection;
         //The structure above can reverse the direction of animation.
         canvas.drawBitmap(character.get(characterFlame), canvas.getWidth() / 4, canvas.getHeight() / 4, null);
-
-        try {
-            Thread.sleep(250);
-        } catch (InterruptedException e) {
-            Thread.interrupted();
-        }
-
-        /*
-        //Draw a lightgray rectangle
-        paintBrush.setColor(Color.parseColor("lightgray"));
-        paintBrush.setStrokeWidth(10);
-        canvas.drawRect(100, 100, 800, 800, paintBrush);
-        */
+        postInvalidateDelayed(250);
     }
 
     public void drawWindow(int width, int height) {
